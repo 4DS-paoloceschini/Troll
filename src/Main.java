235384da4f806e25625b4a50fcd44a22f2634cb4
4DS-paoloceschini <paolo.ceschini@buonarroti.tn.it>
2023-10-04 22:15:10 +0200

    feat: do while per test della classe

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         Troll t1 = new Troll(200, "Gilberto il nano");
-        
+
+        do{
+            System.out.println("Inserisci la spada che vuoi utilizzare tra 1 (20 di danno) e 2 (50 di danno)");
+            int s = input.nextInt();
+            t1.attacco(s);
+            System.out.println(t1.toString());
+        }while(t1.hp>0);
+        System.out.println("Hai sconfitto il troll! :) \uD83E\uDD73");
     }
+
 }
