1d0d24c67e56848e0f286d6abd8c324f050fd4f1
4DS-paoloceschini <paolo.ceschini@buonarroti.tn.it>
2023-10-04 22:06:18 +0200

    feat: istanza della classe Troll con valori di test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Troll t1 = new Troll(200, "Gilberto il nano");
+        
     }
 }
