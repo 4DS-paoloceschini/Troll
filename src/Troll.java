20cba56fe55c3edfb2385c796e1ad8fe36036be4
4DS-paoloceschini <paolo.ceschini@buonarroti.tn.it>
2023-10-04 21:46:09 +0200

    fix: metodo costruttore e inserimento danni delle spade

--- src/Troll.java
+++ src/Troll.java
@@ -1,9 +1,13 @@
 public class Troll {
     int hp;
     String nome;
+    int dannoSpada1;
+    int dannoSpada2;
 
     public Troll (int hp, String nome){
         this.hp = hp;
         this.nome = nome;
+        this.dannoSpada1 = 20;
+        this.dannoSpada2 = 50;
     }
 }
