110c0a5fe08b0b82fcc550de6f1a344911f81665
4DS-paolo.ceschini <paolo.ceschini@buonarroti.tn.it>
2023-10-05 14:01:34 +0200

    feat: inserimento costruttore e setHp

--- src/Eroe.java
+++ src/Eroe.java
@@ -1,4 +1,14 @@
 public class Eroe {
     String nome;
     int hp;
+
+    public Eroe (String nome, int hp){
+        this.nome=nome;
+        this.hp=hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp -= hp;
+    }
+
 }
